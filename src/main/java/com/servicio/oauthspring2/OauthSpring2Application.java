cba4e59b21698ccae30b02714111f93fbfca216b
danicode <danielanzawa@gmail.com>
2024-02-08 19:25:32 -0300

    Initial commit

---
+++ src/main/java/com/servicio/oauthspring2/OauthSpring2Application.java
@@ -0,0 +1,13 @@
+package com.servicio.oauthspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicio/oauthspring2/OauthSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.oauthspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
