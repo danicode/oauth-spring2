93ad92acef0aa5f6e5d16929b4649eada0feb19b
danicode <danielanzawa@gmail.com>
2024-02-12 10:32:33 -0300

    Probando la autenticación con Postman y obteniendo el token JWT

--- src/main/java/com/servicio/oauthspring2/security/SpringSecurityConfig.java
+++ src/main/java/com/servicio/oauthspring2/security/SpringSecurityConfig.java
@@ -20,7 +20,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     private AuthenticationEventPublisher eventPublisher;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
